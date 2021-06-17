da07524cf66fd764377b0d311b35ecdfa92b6047
Đặng Trường Giang <giangdt.kma@gmail.com>
2021-06-17 22:04:29 +0700

    add config name file

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,9 +3,12 @@ export const statusHTTP = {
     FAIL: 500,
     NOT_FOUND: 404,
     UNAUTHORIZED: 401,
+    BAD_REQUEST: 400,
 };
 
 export enum roles {
     STUDENT = 'student',
     TEACHER = 'teacher',
 }
+
+export const fileNamePictures = 'pictures';
